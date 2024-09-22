6edc3741b51c62da582fd35d517cb61e5b73bfb2
akurbanoff <Uspeshniy.ae@yandex.ru>
2024-09-22 14:42:53 +0300

    add request notification permission

--- app/src/main/java/ru/akurbanoff/apptracker/ui/MainActivity.kt
+++ app/src/main/java/ru/akurbanoff/apptracker/ui/MainActivity.kt
@@ -1,5 +1,8 @@
 package ru.akurbanoff.apptracker.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,13 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import dagger.hilt.android.AndroidEntryPoint
 import ru.akurbanoff.apptracker.ui.navigation.MainNavigationGraph
 import ru.akurbanoff.apptracker.ui.theme.AppTrackerTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
     private val mainActivityViewModel: MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,5 +33,23 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        requestNotificationPermission()
+    }
+
+    private fun requestNotificationPermission(){
+        if(Build.VERSION.SDK_INT >= 33 && ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) !=
+            PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.POST_NOTIFICATIONS
+                ),
+                NOTIFICATION_REQUEST_CODE
+            )
+        }
+    }
+
+    companion object{
+        private const val NOTIFICATION_REQUEST_CODE = 112
     }
 }
